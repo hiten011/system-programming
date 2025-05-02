d07e33462b0e8c30e742f01d7c7dd8e263fedf71
hiten <hitengupta12345@gmail.com>
2025-05-02 21:53:38 +0930

    h

--- sequence.c
+++ sequence.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <wait.h>
 
 int main() {
-    while ()
+    
+    // taking input
+    char cmds[100][256]; // max: 100 lines per file and 256 characters per line
+    
+    int count = 0;
+    while (count < 100 && fgets(cmds[count], 256, stdin) != NULL) {
+        // removing the newline character
+        cmds[count][strcspn(cmds[count], "\n")] = '\0'; 
+        count++;
+    }
+
+    // priinting input for confirmation
+    // for (int i = 0; i < count; i++) {
+    //     printf("Line %d: %s\n", i+1, cmds[i]);
+    // }
+
+    // processing the input
+    for (int i = 0; i < count; i++) {
+        int id = fork();
+
+        if (id == 0) {
+            // child process
+            
+        } else {
+            // parent process
+            wait(NULL);
+        }
+    }
+
+
     return 0;
 }
