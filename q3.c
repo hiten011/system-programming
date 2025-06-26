95d476b62dcea1743728a09db85eaf416e2376c5
hiten <hitengupta12345@gmail.com>
2025-06-26 17:00:44 +0930

    h

--- q3.c
+++ q3.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 
 int main() {
-    FILE* output = fopen("disk-usage.txt", write);
-    FILE* error = fopen("du-errors.txt", write);
+    FILE* output = fopen("disk-usage.txt", "w");
+    FILE* error = fopen("du-errors.txt", "w");
 
     int id = fork();
     if (id < 0) {
@@ -17,19 +18,24 @@ int main() {
         // child process
 
         // changing file descriptors
-        dup2(output, 1);
-        dup2(error, 2);
+        dup2(fileno(output), 1);
+        dup2(fileno(error), 2);
 
         // running process
-        char *args[] = {"du", "-sh", "/root/sp", NULL};
+        char *args[] = {"du", "-sh", "/root", NULL};
         execvp(args[0], args);
+
+        // If execvp fails
+        perror("execvp failed");
+        return 1;
     } 
-    
-    else {
-        // parent process
-        wait(NULL);
-        printf("%s\n", "disk usage analysis complete - results have been written to files");
-    }
+
+    // parent process
+    wait(NULL);
+    printf("%s\n", "disk usage analysis complete - results have been written to files");
+
+    fclose(output);
+    fclose(error);
 
     return 0;
 }
